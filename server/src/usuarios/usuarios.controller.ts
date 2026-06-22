54fde569cc100e666db2cb0694791673c1294869
carmen-cespedes <cespedescarmenc@gmail.com>
2026-06-22 05:43:57 -0300

    fot-perfil

--- server/src/usuarios/usuarios.controller.ts
+++ server/src/usuarios/usuarios.controller.ts
@@ -6,42 +6,41 @@ import { AdminGuard } from '../autenticacion/admin.guard';
 import { multerConfig } from '../autenticacion/multer.config';
 
 @Controller('usuarios')
-@UseGuards(TokenGuard, AdminGuard)
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
-  // GET /usuarios
   @Get()
+  @UseGuards(TokenGuard, AdminGuard)
   listar() {
     return this.usuariosService.listar();
   }
 
-  // POST /usuarios
   @Post()
+  @UseGuards(TokenGuard, AdminGuard)
   crear(@Body() datos: any) {
     return this.usuariosService.crear(datos);
   }
 
-  // DELETE /usuarios/:id
   @Delete(':id')
+  @UseGuards(TokenGuard, AdminGuard)
   deshabilitar(@Param('id') id: string) {
     return this.usuariosService.deshabilitar(id);
   }
 
-  // POST /usuarios/:id/habilitar
   @Post(':id/habilitar')
+  @UseGuards(TokenGuard, AdminGuard)
   habilitar(@Param('id') id: string) {
     return this.usuariosService.habilitar(id);
   }
 
-   @Post('foto-perfil')
-    @UseGuards(TokenGuard)
-    @UseInterceptors(FileInterceptor('fotoPerfil', multerConfig))
-    actualizarFoto(
-      @Request() req: any,
-      @UploadedFile() foto: Express.Multer.File,
-    ) {
-      const urlFoto = (foto as any).path;
-      return this.usuariosService.actualizarFoto(req.usuario._id, urlFoto);
-    }
+  @Post('foto-perfil')
+  @UseGuards(TokenGuard)
+  @UseInterceptors(FileInterceptor('fotoPerfil', multerConfig))
+  actualizarFoto(
+    @Request() req: any,
+    @UploadedFile() foto: Express.Multer.File,
+  ) {
+    const urlFoto = (foto as any).path;
+    return this.usuariosService.actualizarFoto(req.usuario._id, urlFoto);
+  }
 }
