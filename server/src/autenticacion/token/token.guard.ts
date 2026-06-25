71abcd8163d5a8379edbf4dce0727250b73b80c5
carmen-cespedes <cespedescarmenc@gmail.com>
2026-06-25 17:01:40 -0300

    token

--- server/src/autenticacion/token/token.guard.ts
+++ server/src/autenticacion/token/token.guard.ts
@@ -6,30 +6,20 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { verify } from 'jsonwebtoken';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Usuario } from '../usuarios.schema';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class TokenGuard implements CanActivate {
-  constructor(
-    @InjectModel(Usuario.name) private usuarioModel: Model<Usuario>
-  ) {}
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const http = context.switchToHttp();
     const req: Request = http.getRequest();
     const authorization = req.headers.authorization;
     const token = authorization?.replace('Bearer ', '') || '';
-    
     try {
       const verificado = verify(token, process.env.CLAVE_SUPERSECRETA!);
       const payload = verificado as { email: string; _id: string; perfil: string };
-
-      const usuario = await this.usuarioModel.findById(payload._id);
-      if (!usuario || !usuario.activo) {
-        throw new UnauthorizedException('Tu cuenta está deshabilitada');
-      }
 
       if (!req.body) {
         req.body = { emailDelToken: payload.email };
@@ -40,6 +30,7 @@ export class TokenGuard implements CanActivate {
       (req as any).usuario = payload;
       return true;
     } catch (error) {
+      console.error(error);
       throw new UnauthorizedException();
     }
   }
